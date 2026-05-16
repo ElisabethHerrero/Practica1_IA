a3006652015096bb9938d5db83a4635d6808118f
Lara :] <lara.duranrioboo@usp.ceu.es>
2026-05-16 21:33:20 +0200

    Añadido tinte rojo para cuando recibes daño
    El juego te avisa de que recibes daño con un tinte rojo

--- Assets/Scripts/UI/VidaUI.cs
+++ Assets/Scripts/UI/VidaUI.cs
@@ -1,16 +1,29 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VidaUI : MonoBehaviour
 {
     public Vida playerVida;
     public TextMeshProUGUI vidaText;
+    public Image pantallaDaño;
 
     void Update()
     {
-        if (playerVida != null && vidaText != null)
-        {
-            vidaText.text = "Vida: " + playerVida.CurrentHealth + " %";
-        }
+        vidaText.text =
+            "Vida: " + playerVida.CurrentHealth + "%";
+
+        float vidaNormalizada =
+            playerVida.CurrentHealth / 100f;
+
+        float daño =
+            1f - vidaNormalizada;
+
+        float alpha =
+            Mathf.Pow(daño, 2f) * 0.4f;
+
+        Color c = pantallaDaño.color;
+        c.a = alpha;
+        pantallaDaño.color = c;
     }
 }
